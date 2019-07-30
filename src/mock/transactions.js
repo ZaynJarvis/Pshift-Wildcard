cf5777cf66a7a0cbcd727b99be616b80412377ce
Yang, Jingyi <jingyyan@visa.com>
2019-07-30 23:55:40 +0800

    updated wallet page content

--- src/mock/transactions.js
+++ src/mock/transactions.js
@@ -1,10 +1,45 @@
 export default [
-	{ id: 0, item: 'X-men', description: "Wolverine's Claws", inflow: true, amount: '30' },
-	{ id: 1, item: 'X-men', description: "Wolverine's Claws", amount: '80' },
-	{ id: 2, item: 'X-men', description: "Wolverine's Claws", inflow: true, amount: '70' },
-	{ id: 3, item: 'X-men', description: "Wolverine's Claws", amount: '80' },
-	{ id: 4, item: 'X-men', description: "Wolverine's Claws", inflow: true, amount: '70' },
-	{ id: 5, item: 'X-men', description: "Wolverine's Claws", inflow: true, amount: '170' },
-	{ id: 6, item: 'X-men', description: "Wolverine's Claws", inflow: true, amount: '700' },
-	{ id: 7, item: 'X-men', description: "Wolverine's Claws", amount: '80' },
+  {
+    id: 0,
+    item: "Angular Frontend project",
+    description: "2019-06-30",
+    inflow: true,
+    amount: "30"
+  },
+  {
+    id: 1,
+    item: "UX design for Ecommerce Website",
+    description: "2018-07-25",
+    amount: "80"
+  },
+  {
+    id: 2,
+    item: "MySQL Database Migration",
+    description: "2017-02-28",
+    inflow: true,
+    amount: "70"
+  },
+  { id: 3, item: "X-men", description: "Wolverine's Claws", amount: "80" },
+  {
+    id: 4,
+    item: "X-men",
+    description: "Wolverine's Claws",
+    inflow: true,
+    amount: "70"
+  },
+  {
+    id: 5,
+    item: "X-men",
+    description: "Wolverine's Claws",
+    inflow: true,
+    amount: "170"
+  },
+  {
+    id: 6,
+    item: "X-men",
+    description: "Wolverine's Claws",
+    inflow: true,
+    amount: "700"
+  },
+  { id: 7, item: "X-men", description: "Wolverine's Claws", amount: "80" }
 ];
