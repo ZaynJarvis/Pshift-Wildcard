aac82f5ea13e5ae5a600f42fde82d5804b29cddf
Yang, Jingyi <jingyyan@visa.com>
2019-07-30 23:50:01 +0800

    updated marketplace content

--- src/mock/project.js
+++ src/mock/project.js
@@ -26,28 +26,27 @@ export default [
   },
   {
     id: 2,
+    title: "UX design for Android App",
+    description:
+      "We are HealthTech company and are looking for an experienced and creative UI/UX Designer to join our team for building a Healthcare Ecosystem. As an UI-UX Designer, you will be responsible for delivering the best online user experience, which makes your role extremely important for our success and ensuring customer satisfaction and loyalty.",
+    tasks: [["a"], ["b", "b"], ["c"]],
+    imageUrl: "https://i.udemycdn.com/course/750x422/1234368_1c41.jpg",
+    conflict: true,
+    status: 2
+  },
+  {
+    id: 3,
     title: "Lorem Ipsum",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     tasks: [["a"], ["b", "b"], ["c"]],
     imageUrl:
       "https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=100",
-    conflict: true,
-    status: 2
-  },
-  {
-    id: 3,
-    title: "React Zeroxac",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    tasks: [["a"], ["b", "b"], ["c"]],
-    imageUrl:
-      "https://images.pexels.com/photos/2182973/pexels-photo-2182973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=100",
     status: 1
   },
   {
     id: 4,
-    title: "Lorem Ipsum",
+    title: "React Zeroxac",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     tasks: [["a"], ["b", "b"], ["c"]],
