860111a7561c73ecd75b659d084ab02bacd3163c
Yang, Jingyi <jingyyan@visa.com>
2019-07-30 17:23:30 +0800

    update image

--- src/mock/project.js
+++ src/mock/project.js
@@ -1,82 +1,88 @@
 export default [
-	{
-		id: 0,
-		title: 'One day build',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		tasks: [
-			['Discussion on Project structure'],
-			['Frontend implement'],
-			['Database Creation', 'ELK implemnetation'],
-			['Deployment'],
-		],
-		imageUrl:
-			'https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=100',
-		status: 0,
-		conflict: true,
-	},
-	{
-		id: 1,
-		title: 'Nodejs backend',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		tasks: [['a'], ['b', 'b'], ['c']],
-		imageUrl:
-			'https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=100',
-		status: 1,
-	},
-	{
-		id: 2,
-		title: 'Lorem Ipsum',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		tasks: [['a'], ['b', 'b'], ['c']],
-		imageUrl:
-			'https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=100',
-		conflict: true,
-		status: 2,
-	},
-	{
-		id: 3,
-		title: 'React Zeroxac',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		tasks: [['a'], ['b', 'b'], ['c']],
-		imageUrl:
-			'https://images.pexels.com/photos/2182973/pexels-photo-2182973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=100',
-		status: 1,
-	},
-	{
-		id: 4,
-		title: 'Lorem Ipsum',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		tasks: [['a'], ['b', 'b'], ['c']],
-		imageUrl:
-			'https://images.pexels.com/photos/2253643/pexels-photo-2253643.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=100',
-		status: 1,
-	},
-	{
-		id: 5,
-		title: 'Telegram Chatbot',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		tasks: [
-			['Discussion on Project structure'],
-			['Frontend implement'],
-			['Database Creation', 'ELK implemnetation'],
-			['Deployment'],
-		],
-		imageUrl:
-			'https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=100',
-		status: 2,
-	},
-	{
-		id: 6,
-		title: 'Project Iceberx',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		tasks: [
-			['Discussion on Project structure'],
-			['Frontend implement'],
-			['Database Creation', 'ELK implemnetation'],
-			['Deployment'],
-		],
-		imageUrl:
-			'https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=100',
-		completed: true,
-	},
+  {
+    id: 0,
+    title: "One day build",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    tasks: [
+      ["Discussion on Project structure"],
+      ["Frontend implement"],
+      ["Database Creation", "ELK implementation"],
+      ["Deployment"]
+    ],
+    imageUrl: "https://miro.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
+    status: 0,
+    conflict: true
+  },
+  {
+    id: 1,
+    title: "Nodejs backend",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    tasks: [["a"], ["b", "b"], ["c"]],
+    imageUrl:
+      "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=100",
+    status: 1
+  },
+  {
+    id: 2,
+    title: "Lorem Ipsum",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    tasks: [["a"], ["b", "b"], ["c"]],
+    imageUrl:
+      "https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=100",
+    conflict: true,
+    status: 2
+  },
+  {
+    id: 3,
+    title: "React Zeroxac",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    tasks: [["a"], ["b", "b"], ["c"]],
+    imageUrl:
+      "https://images.pexels.com/photos/2182973/pexels-photo-2182973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=100",
+    status: 1
+  },
+  {
+    id: 4,
+    title: "Lorem Ipsum",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    tasks: [["a"], ["b", "b"], ["c"]],
+    imageUrl:
+      "https://images.pexels.com/photos/2253643/pexels-photo-2253643.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=100",
+    status: 1
+  },
+  {
+    id: 5,
+    title: "Telegram Chatbot",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    tasks: [
+      ["Discussion on Project structure"],
+      ["Frontend implement"],
+      ["Database Creation", "ELK implemnetation"],
+      ["Deployment"]
+    ],
+    imageUrl:
+      "https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=100",
+    status: 2
+  },
+  {
+    id: 6,
+    title: "Project Iceberx",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    tasks: [
+      ["Discussion on Project structure"],
+      ["Frontend implement"],
+      ["Database Creation", "ELK implemnetation"],
+      ["Deployment"]
+    ],
+    imageUrl:
+      "https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=100",
+    completed: true
+  }
 ];
