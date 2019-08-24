517a190030bf7059918c0c955cc88313397895a6
Jarrett Yeo <jarrettyeo@users.noreply.github.com>
2019-08-24 17:02:59 +0800

    Added react-search-input as dynamic search bar for MarketPage

--- src/containers/MarketPage/index.jsx
+++ src/containers/MarketPage/index.jsx
@@ -1,50 +1,99 @@
-import React from 'react';
-
-import SearchBar from './SearchBar';
-import TagMenu from './TagMenu';
+import React, {Component} from 'react'
 import './style.css';
-
 import Layout from '../Layout';
-import MissionCard from '../../components/MissionCard';
-import Projects from '../../mock/project';
 
-export default () => {
-	const [page, setPage] = React.useState(0);
-
-	return (
-		<Layout title="WildCard">
-			<div className="project-page">
-				<div className="project-content" />
-				<SearchBar page={page} setPage={setPage} />
-				<TagMenu />
-				{page === 0 ? (
-					<>
-						{Projects.filter(p => !p.completed).map((v, i) => (
-							<MissionCard
-								key={i}
-								id={i}
-								title={v.title}
-								description={v.description}
-								imageUrl={v.imageUrl}
-								tasks={v.tasks}
-							/>
-						))}
-					</>
-				) : (
-					<>
-						{Projects.filter(p => p.completed).map((v, i) => (
-							<MissionCard
-								key={i}
-								id={i}
-								title={v.title}
-								description={v.description}
-								imageUrl={v.imageUrl}
-								tasks={v.tasks}
-							/>
-						))}
-					</>
-				)}
-			</div>
+import SearchInput, {createFilter} from 'react-search-input'
+import { Container, Row, Col, Button, Card, ListGroup } from 'react-bootstrap';
+ 
+// import projects from './mails'
+ 
+const KEYS_TO_FILTERS = ['name', 'desc']
+ 
+class Marketplace extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+    this.searchUpdated = this.searchUpdated.bind(this)
+  }
+ 
+  render () {
+	const projects = [
+		{
+			name: "Linux",
+			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+		{
+			name: "React",
+			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+		{
+			name: "Node.JS Backend",
+			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+		{
+			name: "Python Flask",
+			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+		{
+			name: "Web App",
+			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+		{
+			name: "Telegram Chatbot 1",
+			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+		{
+			name: "Mobile App Dev",
+			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+		{
+			name: "Telegram Chatbot 2",
+			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+		},
+	]
+    const filteredProjects = projects.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+ 
+    return (
+		<Layout title="Marketplace">
+			<Container>
+				
+				<Row>
+					<Col>
+						<SearchInput className="search-input form-control" onChange={this.searchUpdated} />
+					</Col>
+				</Row>
+				
+				<Row>
+					{filteredProjects.map(project => {
+						return (
+							<Col md={4}>
+								<Card>
+								<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+								<Card.Body>
+									<Card.Title>{project.name}</Card.Title>
+									<Card.Text>
+										{project.desc}
+									</Card.Text>
+									<Button variant="primary">Apply</Button>
+								</Card.Body>
+								<ListGroup className="list-group-flush">
+									<ListGroup.Item variant="primary">Phase 1 of 4: In Progress</ListGroup.Item>
+								</ListGroup>
+								</Card>	
+							</Col>
+						)
+					})}					
+				</Row>
+			</Container>
 		</Layout>
-	);
-};
+    )
+  }
+ 
+  searchUpdated (term) {
+    this.setState({searchTerm: term})
+  }
+}
+
+export default Marketplace;
