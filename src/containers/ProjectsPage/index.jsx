cde795424407931fe31faa53edf1382cb397c6b7
Jarrett Yeo <jarrettyeo@users.noreply.github.com>
2019-08-24 16:23:36 +0800

    Changed ProjectsPage to use justified Tabs

--- src/containers/ProjectsPage/index.jsx
+++ src/containers/ProjectsPage/index.jsx
@@ -1,128 +1,213 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import './style.css';
 import Layout from '../Layout';
 
-import { Container, Row, Col, ButtonGroup, Button, Card, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card, Carousel, Tabs, Tab, ListGroup } from 'react-bootstrap';
 
-class ProjectPage extends Component {
+export default () => {
+	return (
+		<Layout title="Projects">
+			<Container>
 
-	constructor(props){
-		super(props);
-		this._onButtonChange = this._onButtonChange.bind(this);
-		this.state = {
-			option: "Ongoing",
-		};
-	  }
+				<Row>
+					<Col>
+						<Carousel>
+							<Carousel.Item>
+								<img
+								className="d-block w-100"
+								src="https://source.unsplash.com/800x400/?coding"
+								alt="First slide"
+								/>
+								<Carousel.Caption>
+								<h3>Manage Projects</h3>
+								<p>Here you can manage the milestones for ongoing projects and view completed projects.</p>
+								</Carousel.Caption>
+							</Carousel.Item>
+						</Carousel>
+					</Col>
+				</Row>
 
-	_onButtonChange(option) {
-		this.setState({
-			option: option
-		});
-	}
+				<Row>
+					<Col>
 
-	render() {
-		return (
-			<Layout title="Projects">
-				<Container>
-					
-					<Row>
-						<Col>
-							<ButtonGroup className="centralize">
-								<Button onClick={this._onButtonChange.bind(this, 'Ongoing')} active={this.state.option === 'Ongoing'}>Ongoing</Button>
-								<Button onClick={this._onButtonChange.bind(this, 'Completed')} active={this.state.option === 'Completed'}>Completed</Button>
-							</ButtonGroup>
-						</Col>
-					</Row>
-				
-					<Row>
-						<Col md={4}>
-							<Card>
-							<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
-							<Card.Body>
-								<Card.Title>Card Title</Card.Title>
-								<Card.Text>
-								Some quick example text to build on the card title and make up the bulk of
-								the card's content.
-								</Card.Text>
-								<Button variant="primary">Manage</Button>
-							</Card.Body>
-							<ListGroup className="list-group-flush">
-								<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
-							</ListGroup>
-							</Card>
-						</Col>
-						<Col md={4}>
-							<Card>
-							<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
-							<Card.Body>
-								<Card.Title>Card Title</Card.Title>
-								<Card.Text>
-								Some quick example text to build on the card title and make up the bulk of
-								the card's content.
-								</Card.Text>
-								<Button variant="primary">Manage</Button>
-							</Card.Body>
-							<ListGroup className="list-group-flush">
-								<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
-							</ListGroup>
-							</Card>
-						</Col>
-						<Col md={4}>
-							<Card>
-							<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
-							<Card.Body>
-								<Card.Title>Card Title</Card.Title>
-								<Card.Text>
-								Some quick example text to build on the card title and make up the bulk of
-								the card's content.
-								</Card.Text>
-								<Button variant="primary">Manage</Button>
-							</Card.Body>
-							<ListGroup className="list-group-flush">
-								<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
-							</ListGroup>
-							</Card>
-						</Col>
-						<Col md={4}>
-							<Card>
-							<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
-							<Card.Body>
-								<Card.Title>Card Title</Card.Title>
-								<Card.Text>
-								Some quick example text to build on the card title and make up the bulk of
-								the card's content.
-								</Card.Text>
-								<Button variant="primary">Manage</Button>
-							</Card.Body>
-							<ListGroup className="list-group-flush">
-								<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
-							</ListGroup>
-							</Card>
-						</Col>
-						<Col md={4}>
-							<Card>
-							<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
-							<Card.Body>
-								<Card.Title>Card Title</Card.Title>
-								<Card.Text>
-								Some quick example text to build on the card title and make up the bulk of
-								the card's content.
-								</Card.Text>
-								<Button variant="primary">Manage</Button>
-							</Card.Body>
-							<ListGroup className="list-group-flush">
-								<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
-							</ListGroup>
-							</Card>
-						</Col>
-					</Row>
+						<Tabs fill defaultActiveKey="ongoing" id="uncontrolled-tab-example">
 
-				</Container>
-			</Layout>
-		);
-	}
+							<Tab eventKey="ongoing" title="Ongoing">
+								<Row>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>																								
+								</Row>
+							</Tab>
 
+							<Tab eventKey="completed" title="Completed">
+								<Row>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>
+									<Col md={4}>
+										<Card>
+										<Card.Img variant="top" src="https://source.unsplash.com/180x100/?coding" />
+										<Card.Body>
+											<Card.Title>Card Title</Card.Title>
+											<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+											</Card.Text>
+											<Button variant="primary">Manage</Button>
+										</Card.Body>
+										<ListGroup className="list-group-flush">
+											<ListGroup.Item variant="success">Phase 1 of 4: In Progress</ListGroup.Item>
+										</ListGroup>
+										</Card>
+									</Col>																								
+								</Row>
+							</Tab>
+						
+						</Tabs>
+
+					</Col>
+				</Row>
+
+			</Container>
+		</Layout>
+	);
 };
-
-export default ProjectPage;
