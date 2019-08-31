9dfd1501fe1c9bb0a4fe5cc4e91fdfa4cb175b26
guomk <gmk19990312@gmail.com>
2019-08-31 11:07:40 +0800

    Refine btm navbar

--- src/components/BottomNavigation/index.jsx
+++ src/components/BottomNavigation/index.jsx
@@ -1,30 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './style.css';
-import { Navbar, Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FaWallet, FaProjectDiagram } from 'react-icons/fa';
+import { IoMdPeople, IoIosBrowsers } from 'react-icons/io';
 
 class BottomNavigation extends Component {
-	render() {
-		return (
-			<Navbar fixed="bottom" bg="dark" variant="dark" className="centralize">
-				<Nav className="mr-auto">
-					<LinkContainer to="/market">
-						<Nav.Link>Market</Nav.Link>
-					</LinkContainer>				
-					<LinkContainer to="/projects">
-						<Nav.Link>Projects</Nav.Link>
-					</LinkContainer>
-					<LinkContainer to="/insurance">
-						<Nav.Link>Insurance</Nav.Link>
-					</LinkContainer>
-					<LinkContainer to="/wallet">
-						<Nav.Link>Wallet</Nav.Link>
-					</LinkContainer>					
-				</Nav>
-			</Navbar>
-		)
-	}
+  render() {
+    return (
+      <Navbar
+        fixed='bottom'
+        bg='dark'
+        variant='dark'
+        className='centralize navbar'>
+        <Nav className='mr-auto'>
+          <LinkContainer to='/market'>
+            <Nav.Link className='nav-link'>
+              <IoIosBrowsers />
+              <p>Market</p>
+            </Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/projects'>
+            <Nav.Link>
+              <FaProjectDiagram />
+              <p>Projects</p>
+            </Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/insurance'>
+            <Nav.Link>
+              <IoMdPeople />
+              <p>Insurance</p>
+            </Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/wallet'>
+            <Nav.Link>
+              <FaWallet />
+              <p>Wallet</p>
+            </Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar>
+    );
+  }
 }
-
 
 export default BottomNavigation;
