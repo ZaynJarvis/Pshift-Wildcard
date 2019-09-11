f4142f82c7ad480bd6d7dfd1d39096413c0dcba2
guomk <gmk19990312@gmail.com>
2019-09-12 00:05:15 +0800

    log out content retrieved from API

--- www/src/context/transaction/TransactionState.jsx
+++ www/src/context/transaction/TransactionState.jsx
@@ -6,37 +6,36 @@ import TransactionReducer from './transactionReducer';
 import { GET_ALL_TRANSACTIONS, SET_LOADING } from '../types';
 
 const TransactionState = props => {
-	const initialState = {
-		transactions: [],
-		loading: false,
-	};
+  const initialState = {
+    transactions: [],
+    loading: false
+  };
 
-	const [state, dispatch] = useReducer(TransactionReducer, initialState);
+  const [state, dispatch] = useReducer(TransactionReducer, initialState);
 
-	const getAllTransactions = async () => {
-		setLoading();
-		const res = await axios.get(`http://service/api/transactions`);
-		// console.log(res);
-		dispatch({
-			type: GET_ALL_TRANSACTIONS,
-			payload: res.data,
-		});
-	};
+  const getAllTransactions = async () => {
+    setLoading();
+    const res = await axios.get(`http://service/api/transactions`);
+    console.log(res);
+    dispatch({
+      type: GET_ALL_TRANSACTIONS,
+      payload: res.data
+    });
+  };
 
-	// Set Loading
-	const setLoading = () => dispatch({ type: SET_LOADING });
+  // Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
-	return (
-		<TransactionContext.Provider
-			value={{
-				transactions: state.transactions,
-				loading: state.loading,
-				getAllTransactions,
-			}}
-		>
-			{props.children}
-		</TransactionContext.Provider>
-	);
+  return (
+    <TransactionContext.Provider
+      value={{
+        transactions: state.transactions,
+        loading: state.loading,
+        getAllTransactions
+      }}>
+      {props.children}
+    </TransactionContext.Provider>
+  );
 };
 
 export default TransactionState;
