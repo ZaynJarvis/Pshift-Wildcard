a25ae9edf52b49e2fa02b5d8ca2fcb4a105fea74
Zayn Jarvis <zaynjarvis@gmail.com>
2019-09-12 04:48:53 +0800

    modify host

--- www/src/context/gig/GigState.jsx
+++ www/src/context/gig/GigState.jsx
@@ -15,7 +15,7 @@ const GigState = props => {
 	// Get all gigs
 	const getAllGigs = async () => {
 		setLoading();
-		const res = await axios.get(`${process.env.REACT_API || 'http://localhost'}/api/gigs`);
+		const res = await axios.get(`/api/gigs`);
 		console.log(res);
 		dispatch({
 			type: GET_ALL_GIGS,
