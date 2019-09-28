aa19b598e77e8e14fc4965855738a400774b62c2
Zayn Jarvis <zaynjarvis@gmail.com>
2019-09-28 08:39:21 +0800

    style fix

--- www/src/containers/MilestonePage/index.jsx
+++ www/src/containers/MilestonePage/index.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import './style.css';
-import { Row, Col } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import Layout from '../Layout/index';
 import { withRouter } from 'react-router-dom';
 import Stepper from './Stepper';
 import BottomNavigation from '../../components/BottomNavigation/index';
 
 const MilestonePage = ({ match, history }) => {
-  return (
-    <>
-      <Layout title='Milestone'>
-        <Row>
-          <Col>
-            <Stepper id={match.params.id} />
-            <BottomNavigation />
-          </Col>
-        </Row>
-      </Layout>
-    </>
-  );
+	return (
+		<>
+			<Layout title="Milestone">
+				<Col>
+					<Stepper id={match.params.id} />
+					<BottomNavigation />
+				</Col>
+			</Layout>
+		</>
+	);
 };
 
 export default withRouter(MilestonePage);
