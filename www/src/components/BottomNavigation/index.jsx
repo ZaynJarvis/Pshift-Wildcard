060a7f6171883038d5d190caf93c3262711aab56
Zayn Jarvis <zaynjarvis@gmail.com>
2019-09-29 11:40:56 +0800

    update filter

--- www/src/components/BottomNavigation/index.jsx
+++ www/src/components/BottomNavigation/index.jsx
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import './style.css';
-// import { NavLink, withRouter } from 'react-router-dom';
-// import { FaWallet, FaProjectDiagram } from 'react-icons/fa';
-// import { IoMdPeople, IoIosBrowsers } from 'react-icons/io';
-
-// const BottomNavigation = ({ location }) => {
-// 	const config = [
-// 		{ to: '/market', icon: IoIosBrowsers, label: 'market' },
-// 		{ to: '/projects', icon: FaProjectDiagram, label: 'projects' },
-// 		{ to: '/insurance', icon: IoMdPeople, label: 'insurance' },
-// 		{ to: '/wallet', icon: FaWallet, label: 'wallet' },
-// 	];
-// 	return (
-// 		<div className="bottom-navigation">
-// 			{config.map((c, i) => (
-// 				<NavLink
-// 					className="nav-link"
-// 					activeClassName={location.pathname !== c.to ? null : 'active-route'}
-// 					to={c.to}
-// 					exact
-// 					key={c.to}
-// 				>
-// 					<div>
-// 						{c.icon()}
-// 						<p>{c.label}</p>
-// 					</div>
-// 				</NavLink>
-// 			))}
-// 		</div>
-// 	);
-// };
-
-// export default withRouter(BottomNavigation);
 import React, { Component } from 'react';
 import './style.css';
 import { Navbar, Nav } from 'react-bootstrap';
@@ -40,42 +6,38 @@ import { FaWallet, FaProjectDiagram } from 'react-icons/fa';
 import { IoMdPerson, IoIosBrowsers } from 'react-icons/io';
 
 class BottomNavigation extends Component {
-  render() {
-    return (
-      <Navbar
-        fixed='bottom'
-        bg='light'
-        variant='light'
-        className='centralize navbar'>
-        <Nav className='mr-auto'>
-          <LinkContainer to='/market'>
-            <Nav.Link className='nav-link'>
-              <IoIosBrowsers />
-              <p>Market</p>
-            </Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/projects'>
-            <Nav.Link>
-              <FaProjectDiagram />
-              <p>Projects</p>
-            </Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/wallet'>
-            <Nav.Link>
-              <FaWallet />
-              <p>Wallet</p>
-            </Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/profile'>
-            <Nav.Link>
-              <IoMdPerson />
-              <p>Profile</p>
-            </Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar>
-    );
-  }
+	render() {
+		return (
+			<Navbar fixed="bottom" bg="light" variant="light" className="centralize navbar">
+				<Nav className="mr-auto">
+					<LinkContainer to="/market">
+						<Nav.Link className="nav-link">
+							<IoIosBrowsers />
+							<p>Market</p>
+						</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to="/projects">
+						<Nav.Link>
+							<FaProjectDiagram />
+							<p>Projects</p>
+						</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to="/wallet">
+						<Nav.Link>
+							<FaWallet />
+							<p>Wallet</p>
+						</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to="/profile">
+						<Nav.Link>
+							<IoMdPerson />
+							<p>Profile</p>
+						</Nav.Link>
+					</LinkContainer>
+				</Nav>
+			</Navbar>
+		);
+	}
 }
 
 export default BottomNavigation;
--- www/src/containers/MarketPage/index.jsx
+++ www/src/containers/MarketPage/index.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import './style.css';
 import Layout from '../Layout';
-
+import TagMenu from './TagMenu';
 import SearchInput, { createFilter } from 'react-search-input';
 import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -14,88 +14,85 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 const KEYS_TO_FILTERS = ['title', 'description'];
 
 const MarketPage = () => {
-  const gigContext = useContext(GigContext);
-  const userContext = useContext(UserContext);
+	const gigContext = useContext(GigContext);
+	const userContext = useContext(UserContext);
 
-  const { gigs, updateGig } = gigContext;
-  const { id: uid } = userContext;
+	const { gigs, updateGig } = gigContext;
+	const { id: uid } = userContext;
 
-  const [searchTerm, setTerm] = useState('');
+	const [searchTerm, setTerm] = useState('');
 
-  const searchUpdated = term => {
-    setTerm(term);
-  };
+	const searchUpdated = term => {
+		setTerm(term);
+	};
 
-  const updateLike = (id, like) => {
-    if (like === undefined) {
-      like = [uid];
-      updateGig(id, { like, uid });
-    } else if (like.includes(uid)) {
-      like = like.filter(u => u !== uid);
-      updateGig(id, { like, uid });
-    } else {
-      like = [...like, uid];
-      updateGig(id, { like, uid });
-    }
-  };
+	const updateLike = (id, like) => {
+		if (like === undefined) {
+			like = [uid];
+			updateGig(id, { like, uid });
+		} else if (like.includes(uid)) {
+			like = like.filter(u => u !== uid);
+			updateGig(id, { like, uid });
+		} else {
+			like = [...like, uid];
+			updateGig(id, { like, uid });
+		}
+	};
 
-  const filteredGigs = gigs.filter(createFilter(searchTerm, KEYS_TO_FILTERS));
+	const filteredGigs = gigs.filter(createFilter(searchTerm, KEYS_TO_FILTERS));
 
-  return (
-    <Layout title='Marketplace'>
-      <Container>
-        <Row>
-          <Col>
-            <SearchInput
-              className='search-input form-control'
-              onChange={searchUpdated}
-            />
-          </Col>
-        </Row>
-
-        <Row>
-          {filteredGigs.map((project, i) => {
-            return (
-              <Col md={4} key={project.id + i}>
-                <Card>
-                  <Image
-                    src={project.imageUrl}
-                    noLazyLoad='true'
-                    className='cardImgTop'
-                    height='200'
-                    alt='...'
-                    style={{ objectFit: 'cover' }}></Image>
-                  <Card.Body>
-                    <Card.Title>{project.title}</Card.Title>
-                    <Card.Text>{project.description}</Card.Text>
-                    <div className='links'>
-                      <LinkContainer to={`/offer/${project.id}`}>
-                        <Button variant='outline-info'>Apply</Button>
-                      </LinkContainer>
-                      {project.like !== undefined &&
-                      project.like.includes(uid) ? (
-                        <FontAwesomeIcon
-                          icon={faStar}
-                          onClick={() => updateLike(project.id, project.like)}
-                          className='like'
-                        />
-                      ) : (
-                        <FontAwesomeIcon
-                          icon={faStar}
-                          onClick={() => updateLike(project.id, project.like)}
-                          className='default'
-                        />
-                      )}
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    </Layout>
-  );
+	return (
+		<Layout title="Marketplace">
+			<Container>
+				<Row>
+					<Col>
+						<SearchInput className="search-input form-control" onChange={searchUpdated} />
+					</Col>
+				</Row>
+				<TagMenu />
+				<Row>
+					{filteredGigs.map((project, i) => {
+						return (
+							<Col md={4} key={project.id + i}>
+								<Card>
+									<Image
+										src={project.imageUrl}
+										noLazyLoad="true"
+										className="cardImgTop"
+										height="200"
+										alt="..."
+										style={{ objectFit: 'cover' }}
+									></Image>
+									<Card.Body>
+										<Card.Title>{project.title}</Card.Title>
+										<Card.Text>{project.description}</Card.Text>
+										<div className="links">
+											<LinkContainer to={`/offer/${project.id}`}>
+												<Button variant="outline-info">Apply</Button>
+											</LinkContainer>
+											{project.like !== undefined && project.like.includes(uid) ? (
+												<FontAwesomeIcon
+													icon={faStar}
+													onClick={() => updateLike(project.id, project.like)}
+													className="like"
+												/>
+											) : (
+												<FontAwesomeIcon
+													icon={faStar}
+													onClick={() => updateLike(project.id, project.like)}
+													className="default"
+												/>
+											)}
+										</div>
+									</Card.Body>
+								</Card>
+							</Col>
+						);
+					})}
+				</Row>
+			</Container>
+		</Layout>
+	);
 };
 
 export default MarketPage;
