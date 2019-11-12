8c9471443ce5509b7fb2c3590a9ba654b9308510
Yang, Jingyi <jingyyan@visa.com>
2019-11-12 17:38:06 +0800

    fixed login & register apis

--- backend/controllers/auth/index.ts
+++ backend/controllers/auth/index.ts
@@ -1,35 +1,46 @@
 import passport from 'passport';
-import { User, UserStore } from '../../models';
+import { User } from '../../entity/User';
+import { Conn } from '../../utils/connection';
 
 const uncachedRequire = path => {
     delete require.cache[require.resolve(path)];
     return require(path);
 };
 
-// export const register = async (req, res, next) => {
-//     const user = new User(req.body);
-//     try {
-//         const existedUser = UserStore.getUser(user.email);
-//         if (existedUser) {
-//             res.status(400).send({
-//                 message: `User (${user.email}) already existed, try to login.`
-//             });
-//             return;
-//         }
-//     } catch (err) {
-//         next(err);
-//         return;
-//     }
-//     try {
-//         UserStore.saveUser(user);
-//         res.status(200).send({
-//             message: 'user register success',
-//             id: user.id
-//         });
-//     } catch (err) {
-//         next(err);
-//     }
-// };
+export const register = async (req, res, next) => {
+    const { name, email, description, avatarUrl, password } = req.body;
+    let newUser = new User();
+    newUser.name = name;
+    newUser.email = email;
+    newUser.description = description;
+    newUser.avatarUrl = avatarUrl;
+    newUser.setPassword(password);
+    const connection = await Conn.getInstance();
+    try {
+        let userRepository = connection.getRepository(User);
+        let existedUser: User = await userRepository.findOne({
+            where: { email: newUser.email }
+        });
+        if (existedUser) {
+            res.status(400).send({
+                message: `User (${newUser.email}) already existed, try to login.`
+            });
+            return;
+        }
+    } catch (err) {
+        next(err);
+        return;
+    }
+    try {
+        await connection.manager.save(newUser);
+        res.status(200).send({
+            message: 'user register success',
+            id: newUser.id
+        });
+    } catch (err) {
+        next(err);
+    }
+};
 
 export const login = async (req, res, next) => {
     passport.authenticate('login', async (err, user) => {
